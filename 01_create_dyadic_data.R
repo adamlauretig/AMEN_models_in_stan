6a94508e405e7aa3df3e276f7896c23d0fbc2a91
adamlauretig <lauretig.1@buckeyemail.osu.edu>
2020-03-29 17:47:08 -0400

    correct some typos, start the dyad model

--- 01_create_dyadic_data.R
+++ 01_create_dyadic_data.R
@@ -6,7 +6,7 @@ str(IR90s$dyadvars)
 
 # A function to convert a matrix of pairs to an edgelist
 # Assumes that rows == columns, but will check
-sociomatrix_to_convert <- IR90s$dyadvars[, , 2]
+sociomatrix_to_convert <- IR90s$dyadvars[, , 2] # trade data
 
 matrix_to_edgelist <- function(sociomatrix_to_convert){
   
@@ -34,9 +34,28 @@ matrix_to_edgelist <- function(sociomatrix_to_convert){
   edgelist <- cbind(
     edgelist, 
     rep(seq.int(from = 1, to = sum(edgelist[, 1] < edgelist[, 2])), 2))
+  edgelist <- cbind(edgelist, ifelse(edgelist[, 1] < edgelist[, 2], 1, 2))
+  colnames(edgelist) <- c("sender", "receiver", "y", "dyad_id", "sr_indicator")
+  # for dyad list include a (1, 2) indicator for send/receive
+  # if s < r -> 1, else 2
   
   return(list(edgelist = edgelist, lookup_table = lookup_table, 
-       n_nodes = max(edgelist[, 1]), n_dyads = max(edgelist[, 4]), N = nrow(edgelist)))
+       n_nodes = max(edgelist[, 1]), n_dyads = max(edgelist[, 4]), 
+       N = nrow(edgelist)))
   
 }
 
+data_for_stan <- matrix_to_edgelist(sociomatrix_to_convert)
+
+m1 <- stan(file = "01_srm_stan.stan", 
+     data = list(
+       N = data_for_stan$N,
+       n_nodes = data_for_stan$n_nodes,
+       n_dyads = data_for_stan$n_dyads,
+       sender_id = data_for_stan$edgelist[, 1],
+       receiver_id = data_for_stan$edgelist[, 2],
+       Y = data_for_stan$edgelist[, 3]), chains = 4, iter = 2000, cores = 4)
+
+m1_params <- extract(m1)
+preds <- apply(m1_params$Y_sim, 2, mean)
+plot(data_for_stan$edgelist[, 3], preds)
--- 02_srm_stan_dyad.stan
+++ 02_srm_stan_dyad.stan
@@ -5,9 +5,8 @@ data{
   int N; //total obs. should be n_dyads * 2
   int sender_id[n_dyads * 2] ;
   int receiver_id[n_dyads * 2] ;
-  // int sender_idx[n_dyads * 2] ;
-  // int receiver_idx[n_dyads * 2] ;
-//  int dyad_idx[n_dyads * 2] ;
+  int dyad_id[n_dyads * 2] ;
+  int send_receive[n_dyads * 2] ;
   real Y[n_dyads * 2] ;
   
 }
@@ -16,17 +15,17 @@ parameters{
   cholesky_factor_corr[2] corr_nodes; // correlation matrix w/in hh
   vector<lower=0>[2] sigma_nodes; // sd w/in household
   matrix[2, n_nodes] z_nodes ; // for node non-centered parameterization
-  // cholesky_factor_corr[2] corr_dyads; // correlation matrix w/in hh
-  // real<lower=0> sigma_dyads; // sd w/in household
-  // matrix[2, n_dyads] z_dyads ; // for node non-centered parameterization
+  cholesky_factor_corr[2] corr_dyads; // correlation matrix w/in hh
+  real<lower=0> sigma_dyads; // sd w/in household
+  matrix[2, n_dyads] z_dyads ; // for node non-centered parameterization
 }
 transformed parameters{
-    // matrix[n_dyads,2] mean_dyads; 
+    matrix[n_dyads,2] mean_dyads;
     matrix[n_nodes, 2] mean_nodes; 
     // real linear_predictor[N] ;
     
-    // mean_dyads = (diag_pre_multiply(
-    //   rep_vector(sigma_dyads, 2), corr_dyads) * z_dyads)'; // sd *correlation
+    mean_dyads = (diag_pre_multiply(
+      rep_vector(sigma_dyads, 2), corr_dyads) * z_dyads)'; // sd *correlation
     mean_nodes = (diag_pre_multiply(
       sigma_nodes, corr_nodes) * z_nodes)'; // sd *correlation
     
