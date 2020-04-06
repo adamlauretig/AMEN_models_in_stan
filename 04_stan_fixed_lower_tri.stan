d6e36ddf658b03d7337773658433113cd525831d
adamlauretig <lauretig.1@buckeyemail.osu.edu>
2020-04-05 22:51:23 -0400

    fixed lower triangular matrix

---
+++ 04_stan_fixed_lower_tri.stan
@@ -0,0 +1,67 @@
+
+data{
+  int n_nodes ;
+  int n_dyads ;
+  int N; //total obs. should be n_dyads * 2
+  int sender_id[n_dyads * 2] ;
+  int receiver_id[n_dyads * 2] ;
+  int K ; // number of latent dimensions
+  real Y[n_dyads * 2] ;
+  
+}
+transformed data{
+  int lower_tri_size = (K * (K + 1))/2 ;
+}
+
+parameters{
+  real intercept ;
+  cholesky_factor_corr[2] corr_nodes; // correlation matrix w/in hh
+  vector<lower=0>[2] sigma_nodes; // sd w/in nodes
+  matrix[2, n_nodes] z_nodes ; // for node non-centered parameterization
+  // vector<lower=-pi()/2, upper=pi()/2>[lower_tri_size] cov_raw_values;
+  cholesky_factor_cov[K * 2] cov_multi_effects ; // correlation matrix for multiplicative effect
+  matrix[K * 2, n_nodes] z_multi_effects; // Multi-effect non-centered term 
+  
+}
+transformed parameters{
+    matrix[n_nodes, 2] mean_nodes; // node parameter mean
+    matrix[n_nodes, K * 2] mean_multi_effects ; // multi-effect mean
+    // vector [lower_tri_size] cov_values;
+   
+    // cov_values = 3 * tan(cov_raw_values);
+    
+    mean_nodes = (diag_pre_multiply(
+      sigma_nodes, corr_nodes) * z_nodes)'; // sd *correlation
+    mean_multi_effects = (cov_multi_effects * z_multi_effects)'; // sd *correlation  
+    
+}
+model{
+  intercept ~ normal(0, 5) ;
+  
+  //node terms
+  to_vector(z_nodes) ~ normal(0, 1) ;
+  corr_nodes ~ lkj_corr_cholesky(5) ;
+  sigma_nodes ~ gamma(1, 1) ;
+
+  // multi-effect terms
+  to_vector(z_multi_effects) ~ normal(0, 1) ;
+  to_vector(cov_multi_effects) ~ student_t(3, 0, 2) ;
+
+  for(n in 1:N){
+  Y[n] ~ normal(intercept + 
+        mean_nodes[sender_id[n], 1] + mean_nodes[receiver_id[n], 2] + 
+        mean_multi_effects[sender_id[n], 1:K] * 
+        (mean_multi_effects[receiver_id[n], (K+1):(K*2)])', 
+        1 );
+  }
+}
+generated quantities{
+  real Y_sim[N] ;
+  for(n in 1:N){
+    Y_sim[n] = normal_rng(intercept + 
+        mean_nodes[sender_id[n], 1] + mean_nodes[receiver_id[n], 2] + 
+        mean_multi_effects[sender_id[n], 1:K] * 
+        (mean_multi_effects[receiver_id[n], (K+1):(K*2)])', 1) ;
+  }
+  
+}
---
+++ lower_tri_fixed_amen.R
@@ -0,0 +1,68 @@
+library(amen)
+library(rstan)
+data(IR90s)
+
+
+# A function to convert a matrix of pairs to an edgelist
+# Assumes that rows == columns, but will check
+
+matrix_to_edgelist <- function(sociomatrix_to_convert){
+  
+  if(nrow(sociomatrix_to_convert) != ncol(sociomatrix_to_convert)){
+    stop("nrows != ncols")
+  }
+  
+  all_nodes <- expand.grid(
+    list(rownames(sociomatrix_to_convert), colnames(sociomatrix_to_convert)))
+  all_nodes[, 1] <- as.numeric(all_nodes[, 1])
+  all_nodes[, 2] <- as.numeric(all_nodes[, 2])
+  all_nodes <- all_nodes[all_nodes[, 1] != all_nodes[, 2], ]
+
+  lookup_table <- data.frame(
+    node_names = rownames(sociomatrix_to_convert), 
+    idx = as.numeric(factor(rownames(sociomatrix_to_convert))))
+  obs_values <- which(sociomatrix_to_convert != 0, arr.ind = TRUE)
+  obs_values <- cbind(obs_values, sociomatrix_to_convert[obs_values])
+ 
+   edgelist <- merge(
+    all_nodes, obs_values, by.x = c("Var1", "Var2"), 
+    by.y = c("row", "col"), all.x = TRUE)
+  
+  edgelist$V3 <- ifelse(is.na(edgelist$V3), 0, edgelist$V3)
+  edgelist <- cbind(
+    edgelist, 
+    rep(seq.int(from = 1, to = sum(edgelist[, 1] < edgelist[, 2])), 2))
+  edgelist <- cbind(edgelist, ifelse(edgelist[, 1] < edgelist[, 2], 1, 2))
+  colnames(edgelist) <- c("sender", "receiver", "y", "dyad_id", "sr_indicator")
+  # for dyad list include a (1, 2) indicator for send/receive
+  # if s < r -> 1, else 2
+  
+  return(list(edgelist = edgelist, lookup_table = lookup_table, 
+       n_nodes = max(edgelist[, 1]), n_dyads = max(edgelist[, 4]), 
+       N = nrow(edgelist)))
+  
+}
+
+data_for_stan <- matrix_to_edgelist(IR90s$dyadvars[, , 2]) # trade data
+
+m4_code <- stan_model(file =  "04_stan_fixed_lower_tri.stan"
+  )
+
+m4 <- vb(m4_code, 
+     data = list(
+       N = data_for_stan$N,
+       n_nodes = data_for_stan$n_nodes,
+       n_dyads = data_for_stan$n_dyads,
+       sender_id = data_for_stan$edgelist[, 1],
+       receiver_id = data_for_stan$edgelist[, 2],
+       K = 10,
+       Y = data_for_stan$edgelist[, 3]), 
+     # chains = 4, cores = 4, 
+     iter = 10000, 
+     seed = 123)
+
+m4_params <- extract(m4)
+preds4 <- apply(m4_params$Y_sim, 2, mean)
+plot(data_for_stan$edgelist[, 3], preds4)
+
+mean((data_for_stan$edgelist[, 3] - preds4)^2)
